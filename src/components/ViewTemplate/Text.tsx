4f2233d471a723da831a14fd63bd8c2b31e784a1
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-05-26 12:59:42 +0200

    version peaufiné d'acceuil

--- src/components/ViewTemplate/Text.tsx
+++ src/components/ViewTemplate/Text.tsx
@@ -1,10 +1,15 @@
 import { cn } from "@/lib/utils";
+import LetterPullup from "../magicui/letter-pullup";
 
 /////////////////////////////////////////////////////////////////
-// composant text simple
+// composant text à apparition animé
 const Text:React.FC<{text:string, style:string}> = ({text, style}) => {
     return (
-        <h2 className={cn("text-3.5xl font-bold max-xl:text-xl max-md:text-lg", style)}>{text}</h2>
+        <LetterPullup 
+            className={cn("text-3.5xl font-bold max-xl:text-xl max-md:text-lg", style)}
+            words={text}
+        >
+        </LetterPullup>
 
     )
 }
---
+++ src/components/magicui/letter-pullup.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+interface LetterPullupProps {
+  className?: string;
+  words: string;
+  delay?: number;
+}
+
+export default function LetterPullup({
+  className,
+  words,
+  delay,
+}: LetterPullupProps) {
+  const letters = words.split("");
+  const controls = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: true });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("animate");
+    }
+  }, [inView, controls]);
+
+  const pullupVariant = {
+    initial: { y: 100, opacity: 0 },
+    animate: (i: any) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: i * (delay ? delay : 0.05),
+      },
+    }),
+  };
+
+  return (
+    <div ref={ref} className="flex justify-center">
+      {letters.map((letter, i) => (
+        <motion.h1
+          key={i}
+          variants={pullupVariant}
+          initial="initial"
+          animate={controls}
+          custom={i}
+          className={cn(
+            "font-display text-center text-3.5xl font-bold max-xl:text-xl max-md:text-lg tracking-[-0.02em]",
+            className,
+          )}
+        >
+          {letter === " " ? <span>&nbsp;</span> : letter}
+        </motion.h1>
+      ))}
+    </div>
+  );
+}
