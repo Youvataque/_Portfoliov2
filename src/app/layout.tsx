a0b51cbd019426fdf1b6944034b943df0af19c31
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-05-27 18:44:25 +0200

    mise à jour référencement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Seguin - Website" />
-        <meta property="og:description" content="Un site internet ? Une application mobile ? Ou bien un projet encore plus fou ? Dans tout les cas je suis la solution !" />
+        <meta property="og:description" content="Un site internet ? Une application mobile ? Ou bien un projet encore plus fou ? Contactez moi j'ai surement la solution !" />
         <meta property="og:image" content="/Img/favPres.webp" />
+        <title>Seguin - Website</title>
       </head>
       <body className={cn(inter.className, "bg-background overflow-x-hidden")}>
         <Navbar /> 
