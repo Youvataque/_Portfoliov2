4148c5ae18c07aa2d9cd68c5dffcbc92057529a2
yseguin <youvataque@icloud.com>
2025-08-30 18:16:07 +0200

    test3

--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -1,19 +1,5 @@
+// app/cv/route.ts
 import { NextResponse } from 'next/server';
-import { readFile } from 'fs/promises';
-import { join } from 'path';
-
-export async function GET() {
-  try {
-    const cvPath = join(process.cwd(), 'public', 'cv.pdf');
-    const cvBuffer = await readFile(cvPath);
-    
-    return new NextResponse(cvBuffer, {
-      headers: {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'inline; filename="cv.pdf"'
-      },
-    });
-  } catch (error) {
-    return new NextResponse('CV non trouvé', { status: 404 });
-  }
+export function GET(req: Request) {
+  return NextResponse.redirect(new URL('/cv.pdf', req.url));
 }
